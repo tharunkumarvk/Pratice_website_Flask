7d56437897f3d1bc264176bb919e9d4725fd3603
tharunkumarvk <115857154+tharunkumarvk@users.noreply.github.com>
2024-09-14 08:05:39 +0000

    Frontend done
    Front end part HTML and BOOTSTRAP

--- app.py
+++ app.py
@@ -3,10 +3,42 @@ from flask import render_template #to run the file
 
 app = Flask(__name__) #ap = instance of Flask; __name__=inbuilt name
 
+#to insert dynamic data in html file
+
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location" : "Bengaluru, India",
+        "salary" : "Rs. 10,00,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Enginer",
+        "location" : "Bengaluru, India",
+        "salary" : "Rs. 20,00,000"
+    },
+    {
+        "id": 3,
+        "title": "Front end",
+        "location" : "Chennai, India",
+        "salary" : "Rs. 10,00,000"
+    },
+    {
+        "id": 4,
+        "title": "Backend engineer",
+        "location" : "Bengaluru, India",
+        "salary" : "Rs. 60,00,000"
+    },
+]
+
 @app.route("/") #Routing this to home page (/ = homepage)
 def Hello():
     #return "Hello world"
-    return render_template("home.html")
+    return render_template("home.html",jobs = JOBS)#providing dynamic data as arguement.
+
+
+
 
 #in order to run the above stuffs:
 if __name__ == "__main__":
@@ -14,3 +46,4 @@ if __name__ == "__main__":
 
 #changes in github = version control tab; commit and push
 #templates folder = html files
+#static = Explicit file upload
